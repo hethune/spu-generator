7e856eb0c4237a37313facc0153da003e42f86fa
hethune <wenhangreg@gmail.com>
2020-09-01 21:42:19 +0800

    boostrap check

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,19 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
+import Ping from '../components/Ping.vue';
+import Books from '../components/Books.vue';
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home,
+    path: '/ping',
+    name: 'Ping',
+    component: Ping,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/books',
+    name: 'Books',
+    component: Books,
   },
 ];
 
--- server/app.py
+++ server/app.py
@@ -14,6 +14,31 @@ app.config.from_object(__name__)
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
 
+BOOKS = [
+    {
+        'title': 'On the Road',
+        'author': 'Jack Kerouac',
+        'read': True
+    },
+    {
+        'title': 'Harry Potter and the Philosopher\'s Stone',
+        'author': 'J. K. Rowling',
+        'read': False
+    },
+    {
+        'title': 'Green Eggs and Ham',
+        'author': 'Dr. Seuss',
+        'read': True
+    }
+]
+
+@app.route('/books', methods=['GET'])
+def all_books():
+  return jsonify({
+    'status': 'success',
+    'books': BOOKS
+  })
+
 
 # sanity check route
 @app.route('/ping', methods=['GET'])
